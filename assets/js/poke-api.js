e9e776818a06a6950538f86b0acd1147309c61d2
RafaFonPessoa <rafaelpessoabr@gmail.com>
2023-11-19 00:14:09 -0300

    Projeto Finalizado

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,30 +1,30 @@
 const pokeApi = {};
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
-    pokemon.name = pokeDetail.name
-    pokemon.types = pokeDetail.map((typeSlot) => typeSlot.type.name)
-    pokemon.type = pokemon.types.get[0]
-    pokemon.photo = 
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+    pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.type = pokemon.types[0]; 
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+    return pokemon;
 }
 
 pokeApi.getPokemonDetails = (pokemon) => {
     return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then((pokemon) => {
-
-    })
+        .then((response) => response.json())
+        .then((pokemonDetail) => convertPokeApiDetailToPokemon(pokemonDetail)) // Retorna a instância modificada do Pokémon
+        .catch((error) => console.error(error));
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
-        .then((detalRequests) => Promise.all(detalRequests))
+        .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonDetails) => pokemonDetails)
         .catch((error) => console.error(error));
 };
